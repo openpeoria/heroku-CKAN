50a4b9b506fd88b01e4722d727710f125405f5e6
Reuben Cummings <reubano@gmail.com>
2019-08-07 16:52:11 -0500

    Add info file

---
+++ data-portal.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# vim: sw=4:ts=4:expandtab
+
+"""
+data-portal
+~~~~~~~~~~~
+
+CKAN based Peoria Open Data Portal
+
+"""
+
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals)
+
+__version__ = '0.7.5'
+
+__author__ = 'Reuben Cummings'
+__description__ = 'CKAN based Peoria Open Data Portal'
+__email__ = 'rcummings@nerevu.com'
+__license__ = 'AGPL'
+__copyright__ = 'Copyright 2015 Reuben Cummings'
